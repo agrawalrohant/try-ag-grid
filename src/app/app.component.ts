9f931e4c07d3f72f78d1e46a24522191f1c91331
agrawalrohant <agrawalrohant@gmail.com>
2020-05-09 23:01:31 +0530

    adding simple ag grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-ag-grid';
+
+  columnDefs = [
+    { headerName: 'Make', field: 'make' },
+    { headerName: 'Model', field: 'model' },
+    { headerName: 'Price', field: 'price' },
+  ]
+
+  rowData = [
+    { make: 'Toyoto', model: 'Innova', price: 1200000 },
+    { make: 'Nexa', model: 'Ertica', price: 1000000 },
+    { make: 'Maruti', model: 'Breeza', price: 1100000 }
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { AgGridAngular, AgGridModule } from "ag-grid-angular";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
